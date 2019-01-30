ea513e0df2a64501da9a453ba233748f3c0b65e8
Opolskiy <Opolskiy@bpcbt.com>
2019-01-30 21:46:58 +0300

    Added H2

---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,25 @@
+CREATE TABLE TASKS
+(
+  id int PRIMARY KEY NOT NULL,
+  name varchar(256) NOT NULL,
+  date date,
+  is_urgent int
+);
+
+INSERT INTO tasks VALUES (1, 'test1', '2018-12-31', 0);
+INSERT INTO tasks VALUES (2, 'test2', '2018-12-31', 0);
+INSERT INTO tasks VALUES (3, 'test3', '2018-12-31', 0);
+INSERT INTO tasks VALUES (4, 'test4', '2018-12-31', 0);
+INSERT INTO tasks VALUES (5, 'test5', '2018-12-31', 0);
+INSERT INTO tasks VALUES (6, 'test6', '2018-12-31', 0);
+INSERT INTO tasks VALUES (7, 'test7', '2018-12-31', 0);
+INSERT INTO tasks VALUES (8, 'test8', '2018-12-31', 0);
+INSERT INTO tasks VALUES (9, 'test9', '2018-12-31', 0);
+INSERT INTO tasks VALUES (10, 'test9', '2018-12-31', 0);
+INSERT INTO tasks VALUES (11, 'test9', '2018-12-31', 0);
+INSERT INTO tasks VALUES (12, 'test9', '2018-12-31', 0);
+INSERT INTO tasks VALUES (13, 'test9', '2018-12-31', 0);
+INSERT INTO tasks VALUES (14, 'test9', '2018-12-31', 0);
+INSERT INTO tasks VALUES (15, 'test9', '2018-12-31', 0);
+INSERT INTO tasks VALUES (16, 'test9', '2018-12-31', 0);
+INSERT INTO tasks VALUES (17, 'test9', '2018-12-31', 0);
